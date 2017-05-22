2e914a9266e4fb314f304912daaf699f8d03c37d
Seancheey <adls371@163.com>
2017-05-22 19:22:06 -0400

    Fix initializing components problem in BotEdit

--- src/com/seancheey/gui/BotEdit.kt
+++ src/com/seancheey/gui/BotEdit.kt
@@ -1,19 +1,21 @@
 package com.seancheey.gui
+
 import com.seancheey.game.Component
 import com.seancheey.game.Components
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
-import javafx.scene.layout.*
+import javafx.scene.image.ImageView
+import javafx.scene.layout.BorderPane
+import javafx.scene.layout.FlowPane
 import java.net.URL
 import java.util.*
-import javafx.scene.image.ImageView
 
 /**
-* Created by Seancheey on 20/05/2017.
-* GitHub: https://github.com/Seancheey
-*/
+ * Created by Seancheey on 20/05/2017.
+ * GitHub: https://github.com/Seancheey
+ */
 
-class BotEdit : Initializable{
+class BotEdit : Initializable {
     @FXML
     var borderPane: BorderPane? = null
     @FXML
@@ -23,26 +25,21 @@ class BotEdit : Initializable{
     private var hoverView: ImageView? = null
 
     var holding: Component? = null
-    set(value){
+        set(value) {
             holding = value
             hoverView = ImageView(value?.image)
-    }
-
-    fun sayHi(): Unit {
-        println("Hiiiii")
-    }
+        }
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
-        for(component in Components.blocks){
-            blocksFlowPane?.children?.add(ComponentSlot(component, this))
+        for (component in Components.blocks) {
+            blocksFlowPane!!.children.add(ComponentSlot(component, this))
         }
     }
 }
 
 
-
-class ComponentSlot(component: Component, editController: BotEdit) : ImageView(component.imageURL){
-    init{
-        setOnDragEntered {editController.holding = component}
+class ComponentSlot(component: Component, editController: BotEdit) : ImageView(component.imageURL) {
+    init {
+        setOnDragEntered { editController.holding = component }
     }
 }
