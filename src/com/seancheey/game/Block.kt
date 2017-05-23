ad219b868638b0ac623c8ddbd6ee3e53846da622
Seancheey <adls371@163.com>
2017-05-23 12:18:00 -0400

    Rewrite components kotlin file, clean up file redundancy.

--- src/com/seancheey/game/Block.kt
+++ src/com/seancheey/game/Block.kt
@@ -1,15 +1,11 @@
 package com.seancheey.game
 
 /**
-* Created by Seancheey on 20/05/2017.
-* GitHub: https://github.com/Seancheey
-*/
+ * Created by Seancheey on 20/05/2017.
+ * GitHub: https://github.com/Seancheey
+ */
 
-class Block(name: String, imageURL: String, width: Int, height: Int, health: Int) : Component(name, imageURL, width, height){
-    var health = 0;
-    init{
-        this.health = health
-    }
-    constructor(): this("deault","file:dat/cube.png",10,10,10)
 
+class Block(name: String, imageURL: String, width: Int, height: Int, val health: Int) : Component(name, imageURL, width, height) {
+    constructor() : this("default", "file:dat/cube.png", 10, 10, 10)
 }
--- src/com/seancheey/game/Component.kt
+++ src/com/seancheey/game/Component.kt
@@ -4,60 +4,31 @@ import javafx.geometry.Dimension2D
 import javafx.scene.image.Image
 
 /**
-* Created by Seancheey on 20/05/2017.
-* GitHub: https://github.com/Seancheey
-*/
+ * Created by Seancheey on 20/05/2017.
+ * GitHub: https://github.com/Seancheey
+ */
 
-abstract class Component(name: String, imageURL: String, width: Int, height: Int){
-    var name = ""
+abstract class Component(val name: String, imageURL: String, var width: Int, var height: Int) {
     var image: Image
     var imageURL = ""
-    set(value){
-        field = value
-        if(value != "") {
-            println(value)
-            image = Image(imageURL)
+        set(value) {
+            field = value
+            if (value != "") {
+                image = Image(imageURL)
+            }
         }
-    }
-    private var width = 0
-    private var height = 0
-    val size: Dimension2D
+    var size: Dimension2D
         get() = Dimension2D(width.toDouble(), height.toDouble())
+        set(value) {
+            width = value.width.toInt()
+            height = value.height.toInt()
+        }
 
     init {
-        this.name = name
         this.imageURL = imageURL
-        this.width = width
-        this.height = height
         image = Image(imageURL)
     }
 
-    constructor() : this("Default","cube.png",10,10)
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is Component) return false
-
-        if (name != other.name) return false
-        if (image != other.image) return false
-        if (imageURL != other.imageURL) return false
-        if (width != other.width) return false
-        if (height != other.height) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + image.hashCode()
-        result = 31 * result + imageURL.hashCode()
-        result = 31 * result + width
-        result = 31 * result + height
-        return result
-    }
-
-    override fun toString(): String {
-        return "Component(name='$name', imageURL='$imageURL', width=$width, height=$height)"
-    }
+    constructor() : this("Default", "cube.png", 10, 10)
 
 }
