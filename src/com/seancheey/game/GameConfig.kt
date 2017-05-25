8247bd1a78e78b0a0f49d1cde3f62ef5ae3dc567
Seancheey <adls371@163.com>
2017-05-24 21:33:08 -0400

    Add GameConfig for all game settings

---
+++ src/com/seancheey/game/GameConfig.kt
@@ -0,0 +1,10 @@
+package com.seancheey.game
+
+/**
+ * Created by Seancheey on 24/05/2017.
+ * GitHub: https://github.com/Seancheey
+ */
+object GameConfig {
+    val edit_grid_num = 12
+    val edit_grid_width = 25
+}
--- src/com/seancheey/game/Model.kt
+++ src/com/seancheey/game/Model.kt
@@ -2,14 +2,22 @@ package com.seancheey.game
 
 import javafx.geometry.Dimension2D
 import javafx.scene.image.Image
+import java.io.Serializable
 
 /**
  * Created by Seancheey on 20/05/2017.
  * GitHub: https://github.com/Seancheey
  */
 
-open class Model(val name: String, imageURL: String, var health: Int, var weight: Int, var width: Int, var height: Int) {
+open class Model(val name: String, imageURL: String, var health: Int, var weight: Int, var width: Int, var height: Int) : Serializable {
+    @Transient
     var image: Image
+        get() {
+            if (field == null) {
+                field = Image(imageURL)
+            }
+            return field
+        }
     var imageURL = ""
         set(value) {
             field = value
@@ -30,5 +38,16 @@ open class Model(val name: String, imageURL: String, var health: Int, var weight
     }
 
     constructor() : this("Default", "file:dat/cube.png", 10, 10, 10, 10)
+
+    override fun toString(): String {
+        return "Model(" +
+                "name='$name'," +
+                "health=$health," +
+                "weight=$weight," +
+                "width=$width," +
+                "height=$height," +
+                "imageURL='$imageURL'" +
+                ")"
+    }
 
 }
