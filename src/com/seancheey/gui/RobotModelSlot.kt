9f1503688533b2d4dfe6ed51a4c30b25c6e2f21f
Seancheey <adls371@163.com>
2017-05-29 21:25:50 -0400

    RobotModelSlot are Button now. Styles changed too.

--- src/com/seancheey/gui/RobotModelSlot.kt
+++ src/com/seancheey/gui/RobotModelSlot.kt
@@ -2,15 +2,23 @@ package com.seancheey.gui
 
 import com.seancheey.game.Config
 import com.seancheey.game.RobotModel
+import javafx.scene.control.Button
 import javafx.scene.image.ImageView
 
 /**
  * Created by Seancheey on 27/05/2017.
  * GitHub: https://github.com/Seancheey
  */
-class RobotModelSlot(botModel: RobotModel) : ImageView((botModel.image)) {
+class RobotModelSlot(botModel: RobotModel) : Button("") {
     init {
-        fitWidth = Config.botWidth
-        fitHeight = Config.botWidth
+        val botView = ImageView(botModel.image)
+        botView.fitWidth = Config.botWidth
+        botView.fitHeight = Config.botWidth
+        graphic = botView
+
+        maxWidth = Config.botWidth
+        maxHeight = Config.botWidth
+        minWidth = Config.botWidth
+        minHeight = Config.botWidth
     }
 }
