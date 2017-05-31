a7678f2afff979f231732a7de581ed4e87144e17
Seancheey <adls371@163.com>
2017-05-31 14:41:58 -0400

    seperate modelCopyFormat and BotEdit

---
+++ src/com/seancheey/gui/modelCopyFormat.kt
@@ -0,0 +1,11 @@
+package com.seancheey.gui
+
+import javafx.scene.input.DataFormat
+
+/**
+ * Created by Seancheey on 31/05/2017.
+ * GitHub: https://github.com/Seancheey
+ */
+
+/** DataFormat of componentModel, used as a reference to componentModel in dragging clipboard **/
+object modelCopyFormat : DataFormat("object/componentModel")
