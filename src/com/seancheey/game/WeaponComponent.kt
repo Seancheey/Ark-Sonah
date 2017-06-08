130a06c4de7615de69849e6e5d2d35e13227a87b
Seancheey <adls371@163.com>
2017-06-07 20:34:27 -0400

    Add two component types.

---
+++ src/com/seancheey/game/WeaponComponent.kt
@@ -0,0 +1,7 @@
+package com.seancheey.game
+
+/**
+ * Created by Seancheey on 07/06/2017.
+ * GitHub: https://github.com/Seancheey
+ */
+class WeaponComponent(model: WeaponModel, x: Int, y: Int) : DefaultComponent(model, x, y)
