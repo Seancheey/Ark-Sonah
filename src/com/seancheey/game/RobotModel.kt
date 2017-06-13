78ce0bc8a6dfe7ac95a4a8b723425cce59bd2188
Seancheey <adls371@163.com>
2017-06-13 19:30:33 -0400

    add validating method.

--- src/com/seancheey/game/RobotModel.kt
+++ src/com/seancheey/game/RobotModel.kt
@@ -131,4 +131,49 @@ open class RobotModel(var name: String, val components: List<DefaultComponent>) 
         return result
     }
 
+    fun verify(): ArrayList<WrongMessage> {
+        return Verifier().verify()
+    }
+
+    data class Point(val x: Int, val y: Int)
+    data class WrongMessage(val message: String, val points: ArrayList<Point> = arrayListOf())
+
+    private inner class Verifier {
+        private val verifyList: ArrayList<() -> WrongMessage?> = arrayListOf()
+
+        init {
+            // verify movable
+            verifyList.add {
+                if (components.any { it is MovementComponent }) null else WrongMessage("The robot can't move! put some movement components")
+            }
+            // verify overlap
+            verifyList.add {
+                val overlapPoints: ArrayList<Point> = arrayListOf()
+                val pointMap: HashMap<Point, DefaultComponent> = hashMapOf()
+                for (comp in components) {
+                    val point = Point(comp.gridX, comp.gridY)
+                    if (pointMap.containsKey(point)) {
+                        // except for weapon & mount
+                        if (comp.model.attributes.contains(Attribute.weapon_mount) && pointMap[point]!!.model is WeaponModel) {
+                            continue
+                        }
+                        if (pointMap[point]!!.model.attributes.contains(Attribute.weapon_mount) && comp.model is WeaponModel) {
+                            continue
+                        }
+                        if (!overlapPoints.contains(point))
+                            overlapPoints.add(point)
+                    } else {
+                        pointMap.put(point, comp)
+                    }
+                }
+                if (overlapPoints.isEmpty()) null else WrongMessage("Overlapped components found", overlapPoints)
+            }
+        }
+
+        fun verify(): ArrayList<WrongMessage> {
+            val messages = arrayListOf<WrongMessage>()
+            verifyList.mapNotNullTo(messages) { it() }
+            return messages
+        }
+    }
 }
