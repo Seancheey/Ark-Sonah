aefa7657cf24baece922421640aa292368184156
Seancheey <adls371@163.com>
2017-06-14 17:42:38 -0400

    add playerInGame class.

---
+++ src/com/seancheey/game/PlayerInGame.kt
@@ -0,0 +1,13 @@
+package com.seancheey.game
+
+import com.seancheey.game.battlefield.Battlefield
+
+/**
+ * Created by Seancheey on 14/06/2017.
+ * GitHub: https://github.com/Seancheey
+ */
+data class PlayerInGame(val player: Player, val robotGroupUsed: RobotModelGroup, val battlefield: Battlefield, val side: Side) {
+    enum class Side {
+        side1, side2, side3, side4
+    }
+}
