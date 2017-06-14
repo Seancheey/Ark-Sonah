a05b6f0c5bdd3ac2a5bc2c95144537dd63a72e54
Seancheey <adls371@163.com>
2017-06-14 17:35:56 -0400

    remove player's battlefield field.

--- src/com/seancheey/game/Player.kt
+++ src/com/seancheey/game/Player.kt
@@ -1,6 +1,5 @@
 package com.seancheey.game
 
-import com.seancheey.game.battlefield.Battlefield
 import com.seancheey.io.PlayerSavesWriter
 import java.io.Serializable
 
@@ -8,12 +7,22 @@ import java.io.Serializable
  * Created by Seancheey on 26/05/2017.
  * GitHub: https://github.com/Seancheey
  */
+/**
+ * @param id identity id used to distinguish different players
+ * @param name the displayed name of player
+ * @param pass_SHA the SHA256 of password used to identify player
+ * @param robotGroups all robot models the player has
+ */
+data class Player(val id: Long, var name: String, val pass_SHA: ByteArray, val robotGroups: ArrayList<RobotModelGroup>) : Serializable {
 
-class Player(val id: Long, var name: String, val pass_SHA: ByteArray, val robotGroups: ArrayList<RobotModelGroup>) : Serializable {
-    var battlefield: Battlefield? = null
-
+    /**
+     * used to initiate a new player
+     */
     constructor(id: Long, name: String, pass_SHA: ByteArray) : this(id, name, pass_SHA, arrayListOf(RobotModelGroup(arrayListOf())))
 
+    /**
+     * save the player's data into file with path
+     */
     fun saveData(path: String = Config.playerSavePath(name)) {
         PlayerSavesWriter(this).write(path)
     }
