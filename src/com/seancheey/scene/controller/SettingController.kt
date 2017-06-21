f48b3b59ba210e73a0e804a4daf18fca96f71044
Seancheey <adls371@163.com>
2017-06-21 10:26:49 -0400

    add arrow image to setting.

--- src/com/seancheey/scene/controller/SettingController.kt
+++ src/com/seancheey/scene/controller/SettingController.kt
@@ -1,8 +1,13 @@
 package com.seancheey.scene.controller
 
+import com.seancheey.resources.Resources
 import com.seancheey.scene.Scenes
 import com.seancheey.scene.Stages
+import javafx.fxml.FXML
 import javafx.fxml.Initializable
+import javafx.scene.control.Button
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
 import java.net.URL
 import java.util.*
 
@@ -11,7 +16,14 @@ import java.util.*
  * GitHub: https://github.com/Seancheey
  */
 class SettingController : Initializable {
-    override fun initialize(location: URL?, resources: ResourceBundle?) {}
+    @FXML
+    var menuButton: Button? = null
+
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+        val arrowImage = ImageView(Image(Resources.arrowImageInStream, 30.0, 30.0, false, false))
+        arrowImage.rotate = 270.0
+        menuButton!!.graphic = arrowImage
+    }
 
     fun menuButtonPressed() {
         Stages.switchScene(Scenes.menu)
