bf6e7a691b6228ed8fde925ef0e7685109f8feb6
Seancheey <adls371@163.com>
2017-06-21 23:19:07 -0400

    add a new robot select pane.

--- src/com/seancheey/gui/BotSelectCirclePane.kt
+++ src/com/seancheey/gui/BotSelectCirclePane.kt
@@ -2,6 +2,7 @@ package com.seancheey.gui
 
 import com.seancheey.game.RobotModel
 import javafx.collections.ObservableList
+import javafx.geometry.Point2D
 import javafx.scene.Node
 import javafx.scene.layout.Pane
 import javafx.scene.shape.Circle
@@ -11,7 +12,7 @@ import javafx.scene.shape.Shape
  * Created by Seancheey on 21/06/2017.
  * GitHub: https://github.com/Seancheey
  */
-class BotSelectCirclePane(val models: ArrayList<RobotModel>) : Pane() {
+class BotSelectCirclePane(models: ArrayList<RobotModel>, onClick: (RobotModel) -> Unit = {}) : Pane() {
     var outerCircleRadius: Double = 300.0
         set(value) {
             field = value
@@ -26,7 +27,21 @@ class BotSelectCirclePane(val models: ArrayList<RobotModel>) : Pane() {
     init {
         width = outerCircleRadius * 2
         height = outerCircleRadius * 2
-        
+        RobotModelSlot.allAllTo(children, models, onClick)
+        children.forEachIndexed { i, node ->
+            if (node is RobotModelSlot) {
+                val centerPoint = centerPoint(i)
+                node.layoutX = centerPoint.x - node.width / 2
+                node.layoutY = centerPoint.y - node.height / 2
+            }
+        }
+    }
+
+    private fun centerPoint(i: Int): Point2D {
+        val nodeSum = children.size
+        val fraction = i / nodeSum * 2 * Math.PI
+        val midRadius = (outerCircleRadius + innerCircleRadius) / 2
+        return Point2D(midRadius * Math.cos(fraction), midRadius * Math.sin(fraction))
     }
 
     private fun clipPane() {
