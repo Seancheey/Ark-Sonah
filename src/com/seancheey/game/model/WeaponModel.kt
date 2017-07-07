a93704083d56867ffcda9b79480b6fee54289223
Seancheey <adls371@163.com>
2017-07-07 18:07:29 +0800

    add turnSpeed to weapon model.

--- src/com/seancheey/game/model/WeaponModel.kt
+++ src/com/seancheey/game/model/WeaponModel.kt
@@ -7,20 +7,20 @@ import javax.json.JsonObject
  * Created by Seancheey on 23/05/2017.
  * GitHub: https://github.com/Seancheey
  */
-class WeaponModel(name: String, imageURL: String, health: Int, weight: Int, gridWidth: Int, gridHeight: Int, price: Int, visualWidth: Int, visualHeight: Int, val damage: Int, val range: Int, val frequency: Int) : ComponentModel(name, imageURL, health, weight, gridWidth, gridHeight, price, visualWidth, visualHeight) {
+class WeaponModel(name: String, imageURL: String, health: Int, weight: Int, gridWidth: Int, gridHeight: Int, price: Int, visualWidth: Int, visualHeight: Int, val damage: Int, val range: Int, val frequency: Int, val turnSpeed: Double) : ComponentModel(name, imageURL, health, weight, gridWidth, gridHeight, price, visualWidth, visualHeight) {
 
-    constructor(defaultModel: ComponentModel, damage: Int, range: Int, frequency: Int) : this(defaultModel.name, defaultModel.imageURL, defaultModel.health, defaultModel.weight, defaultModel.gridWidth, defaultModel.gridHeight, defaultModel.price, defaultModel.visualWidth, defaultModel.visualHeight, damage, range, frequency)
+    constructor(defaultModel: ComponentModel, damage: Int, range: Int, frequency: Int, turnSpeed: Double) : this(defaultModel.name, defaultModel.imageURL, defaultModel.health, defaultModel.weight, defaultModel.gridWidth, defaultModel.gridHeight, defaultModel.price, defaultModel.visualWidth, defaultModel.visualHeight, damage, range, frequency, turnSpeed)
 
     init {
-        actionTree.putAction(Action.rotateAction(0.02), Action.SHOOT_ACTION)
+        actionTree.putAction(Action.rotateAction(turnSpeed), Action.SHOOT_ACTION)
     }
 
     companion object {
-        private val keys = listOf("damage", "range", "frequency")
+        private val keys = listOf("damage", "range", "frequency", "turnSpeed")
         fun create(j: JsonObject): WeaponModel? {
             if (!(keys + ComponentModel.requiredKeys).any { j.isNull(it) }) {
                 val defaultModel = ComponentModel.create(j)!!
-                return WeaponModel(defaultModel, j.getInt(keys[0]), j.getInt(keys[1]), j.getInt(keys[2]))
+                return WeaponModel(defaultModel, j.getInt(keys[0]), j.getInt(keys[1]), j.getInt(keys[2]), j.getJsonNumber(keys[3]).doubleValue())
             }
             return null
         }
